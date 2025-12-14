12907aa6b02011a549ba6b9fc922f014b548d7dd
Samyog Maharjan <msamyog37@gmail.com>
2025-12-13 22:31:36 -0600

    Added Dashboard

--- Snapnest/account/urls.py
+++ Snapnest/account/urls.py
@@ -4,7 +4,7 @@ from .import views
 
 urlpatterns=[
     path('login/',auth_views.LoginView.as_view(),name='login'),
-    path('logout/',auth_views.LogoutView(),name='logout'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout'),
 
     path('',views.dashboard,name='dashboard'),
 ]
