8b7af52e438cfcff2627bf3a6ffc3a82b92fc0a1
Samyog Maharjan <msamyog37@gmail.com>
2025-12-15 11:09:45 -0600

    Added following system,

--- Snapnest/account/views.py
+++ Snapnest/account/views.py
@@ -5,8 +5,16 @@ from .forms import LoginForm,UserRegistrationForm,UserEditForm,ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
+from django.contrib.auth import authenticate,get_user_model,login
+from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_POST
+from .models import Contact, Profile
 
 # Create your views here.
+
+
+User=get_user_model()
 
 @login_required
 def dashboard(request):
@@ -101,3 +109,45 @@ def edit(request):
             'profile_form':profile_form
         }
     )
+
+@login_required
+def user_list(request):
+    users=User.objects.filter(is_active=True)
+    return render(
+        request,
+        'account/user/list.html',
+        {'section':'people','users':users}
+
+    )
+
+@login_required
+def user_detail(request,username):
+    user=get_object_or_404(User,username=username,is_active=True)
+    return render(
+        request,
+        'account/user/detail.html',
+        {'section':'people','user':user}
+    )
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id=request.POST.get('id')
+    action=request.POST.get('action')
+    if user_id and action:
+        try:
+            user=User.objects.get(id=user_id)
+            if action =='follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user,
+                    user_to=user
+                ).delete()
+            return JsonResponse({'status':'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status':'error'})
+    return JsonResponse({'status':'error'})
